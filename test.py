96c9301d872292acab8aa2b9f35940b62d8e61ae
Mustafa Gezen <mustafa@norwaykarate.no>
2014-04-10 12:57:29 +0200

    Add detailed information on test.py

--- test.py
+++ test.py
@@ -1,11 +1,19 @@
 from lib.bring import Bring
 
 def main():
+    # Bring object
     bring = Bring()
+
+    # Tracking test
     tracking = bring.tracking("TESTPACKAGE-AT-PICKUPPOINT")
+
+    # You can get this information: (WEIGHT_KG, STATUS, CITY, DESCRIPTION, COUNTRY, COUNTRY_CODE, UNIT_TYPE, POSTAL_CODE, SIGNATURE)
     print tracking.CITY
 
+    # Postal test
     postal = bring.postal("NO", 5055)
+
+    #You can get this information: (RESULT, POSTAL_TYPE)
     print postal.RESULT
 
 if __name__ == "__main__":
