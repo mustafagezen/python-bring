d44661136341c00a6f9e831182aa4c18160d0691
gitless <mustafa@norwaykarate.no>
2014-04-12 02:19:01 +0200

    Nearest pickup point added and better test file

--- lib/bring.py
+++ lib/bring.py
@@ -2,61 +2,117 @@ import requests, json
 from struct import Struct
 
 class Bring(object):
-    BRING_TRACKING_API =    "http://sporing.bring.no/sporing.json?q=%s"
-    BRING_LOCATION_API =    "http://fraktguide.bring.no/fraktguide/api/postalCode.json?country=%s&pnr=%s"
+  BRING_TRACKING_API =    "http://sporing.bring.no/sporing.json?q=%s"
+  BRING_LOCATION_API =    "http://fraktguide.bring.no/fraktguide/api/postalCode.json?country=%s&pnr=%s"
+  BRING_POSTAL_API =   "http://fraktguide.bring.no/fraktguide/api/pickuppoint/postalcode/%s.json"
+  BRING_POSTAL_GEO_API =   "http://fraktguide.bring.no/fraktguide/api/pickuppoint/location/%s/%s.json"
 
-    def tracking(self, tr_nr):
-        """Returns tracking data
-            DATA: (WEIGHT_KG, STATUS, CITY, DESCRIPTION, COUNTRY, COUNTRY_CODE, UNIT_TYPE, POSTAL_CODE, SIGNATURE)
+  def tracking(self, tr_nr):
+    """Returns tracking data
+    DATA(WEIGHT_KG, STATUS, CITY, DESCRIPTION, COUNTRY, COUNTRY_CODE, UNIT_TYPE, POSTAL_CODE, SIGNATURE) and RAW
 
-            @param :tr_nr Tracking number
-        """
-        FINAL_URI = self.BRING_TRACKING_API % tr_nr
-        REQUEST = requests.get(FINAL_URI)
+    @param :tr_nr Tracking number
+    """
+    FINAL_URI = self.BRING_TRACKING_API % tr_nr
+    REQUEST = requests.get(FINAL_URI)
 
-        JSON = json.loads(REQUEST.text)["consignmentSet"][0]
-        PACKAGE_SET = JSON["packageSet"][0]
-        EVENT_SET = PACKAGE_SET["eventSet"][0]
+    JSON = json.loads(REQUEST.text)["consignmentSet"][0]
+    PACKAGE_SET = JSON["packageSet"][0]
+    EVENT_SET = PACKAGE_SET["eventSet"][0]
 
-        DATA = {
-            'WEIGHT_KG': JSON["totalWeightInKgs"],
-            'STATUS': EVENT_SET["status"],
-            'CITY': EVENT_SET["city"],
-            'DESCRIPTION': EVENT_SET["description"],
-            'COUNTRY': EVENT_SET["country"],
-            'COUNTRY_CODE': EVENT_SET["countryCode"],
-            'UNIT_TYPE': EVENT_SET["unitType"],
-            'POSTAL_CODE': EVENT_SET["postalCode"],
-            'SIGNATURE': EVENT_SET["recipientSignature"]["name"]
-        }
+    DATA = {
+    'WEIGHT_KG': JSON["totalWeightInKgs"],
+    'STATUS': EVENT_SET["status"],
+    'CITY': EVENT_SET["city"],
+    'DESCRIPTION': EVENT_SET["description"],
+    'COUNTRY': EVENT_SET["country"],
+    'COUNTRY_CODE': EVENT_SET["countryCode"],
+    'UNIT_TYPE': EVENT_SET["unitType"],
+    'POSTAL_CODE': EVENT_SET["postalCode"],
+    'SIGNATURE': EVENT_SET["recipientSignature"]["name"],
+    'RAW': json.loads(REQUEST.text)
+    }
 
-        for (i, val) in enumerate(DATA):
-            if not DATA[val]:
-                DATA[val] = None
+    for (i, val) in enumerate(DATA):
+      if not DATA[val]:
+        DATA[val] = None
 
-        return Struct(DATA)
+    return Struct(DATA)
 
-    def postal(self, country, postal):
-        """Returns postal information
-            DATA: (RESULT, POSTAL_TYPE)
+  def postal(self, country, postal):
+    """Returns postal information
+    DATA(RESULT, POSTAL_TYPE) and RAW
 
-            @param :country Country Code
-            @param :postal Postal code
-        """
-        FINAL_URI = self.BRING_LOCATION_API % (country, postal)
-        REQUEST = requests.get(FINAL_URI)
+    @param :country Country Code
+    @param :postal Postal code
+    """
+    FINAL_URI = self.BRING_LOCATION_API % (country, postal)
+    REQUEST = requests.get(FINAL_URI)
 
-        JSON = json.loads(REQUEST.text)
+    JSON = json.loads(REQUEST.text)
 
-        DATA = {
-            'RESULT': None,
-            'POSTAL_TYPE': None
-        }
+    DATA = {
+    'RESULT': None,
+    'POSTAL_TYPE': None,
+    'RAW': None
+    }
 
-        if JSON["valid"] == False:
-            pass
-        else:
-            DATA["RESULT"] = JSON["result"]
-            DATA["POSTAL_TYPE"] = JSON["postalCodeType"]
+    if JSON["valid"] == False:
+      pass
+    else:
+      DATA["RESULT"] = JSON["result"]
+      DATA["POSTAL_TYPE"] = JSON["postalCodeType"]
+      DATA["RAW"] = json.loads(REQUEST.text)
 
-        return Struct(DATA)
+    return Struct(DATA)
+
+  def pickup(self, postal=None, geo=None):
+    """Returns the nearest pickup point
+    DATA(NAME, ADDRESS, POSTAL, CITY, COUNTRY_CODE, COUNTY, VISITING_ADDRESS, VISITING_POSTAL, LOCATION_DESCRIPTION, OPENING_HOURS_NO, OPENING_HOURS_EN, RAW)
+
+    @param(optional) :postal Postal code
+    @param(optional) :geo Geographic location in array, ex. [59.7263078,10.2250463]
+
+    @required :postal or :geo
+    """
+    DATA = {
+      'NAME': None,
+      'ADDRESS': None,
+      'POSTAL': None,
+      'CITY': None,
+      'COUNTRY_CODE': None,
+      'COUNTY': None,
+      'VISITING_ADDRESS': None,
+      'VISITING_POSTAL': None,
+      'LOCATION_DESCRIPTION': None,
+      'OPENING_HOURS_NO': None,
+      'OPENING_HOURS_EN': None,
+      'RAW': None
+    }
+
+    if postal != None and geo != None:
+      raise Exception("You cannot provide both postal code and geographic locations!")
+    elif postal != None:
+      FINAL_URI = self.BRING_POSTAL_API % postal
+    elif geo != None:
+      FINAL_URI = self.BRING_POSTAL_GEO_API % (geo[0], geo[1])
+    else:
+      raise Exception("Unexpected error")
+
+    REQUEST = requests.get(FINAL_URI)
+    JSON = json.loads(REQUEST.text)["pickupPoint"][0]
+
+    DATA["NAME"] = JSON["name"]
+    DATA["ADDRESS"] = JSON["address"]
+    DATA["POSTAL"] = JSON["postalCode"]
+    DATA["CITY"] = JSON["city"]
+    DATA["COUNTRY_CODE"] = JSON["countryCode"]
+    DATA["COUNTY"] = JSON["county"]
+    DATA["VISITING_ADDRESS"] = JSON["visitingAddress"]
+    DATA["VISITING_POSTAL"] = JSON["visitingPostalCode"]
+    DATA["LOCATION_DESCRIPTION"] = JSON["locationDescription"]
+    DATA["OPENING_HOURS_NO"] = JSON["openingHoursNorwegian"]
+    DATA["OPENING_HOURS_EN"] = JSON["openingHoursEnglish"]
+    DATA["RAW"] = JSON
+
+    return Struct(DATA)
--- test.py
+++ test.py
@@ -1,20 +1,32 @@
 from lib.bring import Bring
 
 def main():
-    # Bring object
-    bring = Bring()
+  # Bring object
+  bring = Bring()
 
-    # Tracking test
-    tracking = bring.tracking("TESTPACKAGE-AT-PICKUPPOINT")
+  # Tracking test
+  tracking = bring.tracking("TESTPACKAGE-AT-PICKUPPOINT")
 
-    # You can get this information: (WEIGHT_KG, STATUS, CITY, DESCRIPTION, COUNTRY, COUNTRY_CODE, UNIT_TYPE, POSTAL_CODE, SIGNATURE)
-    print tracking.CITY
+  # Returns DATA(WEIGHT_KG, STATUS, CITY, DESCRIPTION, COUNTRY, COUNTRY_CODE, UNIT_TYPE, POSTAL_CODE, SIGNATURE, RAW)
+  print tracking.WEIGHT_KG
 
-    # Postal test
-    postal = bring.postal("NO", 5055)
+  # Postal test
+  postal = bring.postal("NO", 5055)
 
-    #You can get this information: (RESULT, POSTAL_TYPE)
-    print postal.RESULT
+  # Returns DATA(RESULT, POSTAL_TYPE, RAW)
+  print postal.RESULT
+
+  # Nearest pickup point - POSTAL
+  pickup = bring.pickup(3041)
+
+  # Returns DATA(NAME, ADDRESS, POSTAL, CITY, COUNTRY_CODE, COUNTY, VISITING_ADDRESS, VISITING_POSTAL, LOCATION_DESCRIPTION, OPENING_HOURS_NO, OPENING_HOURS_EN, RAW)
+  print pickup.NAME
+
+  # Nearest pickup point - GEO
+  pickup = bring.pickup(None, [59.7263078,10.2250463])
+  print pickup.NAME
+
+
 
 if __name__ == "__main__":
-    main()
+  main()
